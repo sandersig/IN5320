519ceb308274218346d8b5303b26dd7956992001
Sander Sigmundstad <sanderls@icloud.com>
2022-09-02 15:35:20 +0200

    Added close-button to each list-element

--- list.js
+++ list.js
@@ -1,13 +1,17 @@
 
 function append_item() {
     var node = document.createElement("li");
+
     var country = document.getElementById("country");
     var textnode = document.createTextNode(country.value);
+    var cross = '<button class=" cross-button fa-solid fa-circle-xmark" onclick=delete_item()></button>';
     node.appendChild(textnode);
+    node.innerHTML += cross;
     document.getElementById("list").appendChild(node);
     country.value = '';
 }
 
 function delete_item() {
-
+    var list = document.getElementById("list");
+    list.removeChild(list);
 }
