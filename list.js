eab5be176646c53b1f43205bf661a3b5175a67d4
Sander Sigmundstad <sanderls@icloud.com>
2022-09-07 13:06:32 +0200

    Added population to each of the countries when added to the list.

--- list.js
+++ list.js
@@ -1,18 +1,30 @@
-const append_item = () => {
+const append_item = async () => {
     var node = document.createElement("li");
 
     var country = document.getElementById("country");
     var textnode = document.createTextNode(country.value);
+
+    const pop = await getPopulation(country.value);
+    var popnode = document.createElement("span");
+    popnode.innerText = "- " + pop;
+    popnode.setAttribute("class", "population");
 
     var button_node = document.createElement("button");
     button_node.setAttribute("onclick", "this.parentNode.remove()");
     button_node.setAttribute("class", "cross-button fa-solid fa-circle-xmark");
 
     node.appendChild(textnode);
+    node.appendChild(popnode);
     node.appendChild(button_node);
 
     document.getElementById("list").appendChild(node);
     country.value = '';
+}
+
+async function getPopulation(country) {
+    return fetch('https://d6wn6bmjj722w.population.io/1.0/population/' + country + '/today-and-tomorrow/')
+                .then(res => res.json())
+                .then((data) => data.total_population[0].population);
 }
 
 const check_starts_with = (element, searchWord) =>{
