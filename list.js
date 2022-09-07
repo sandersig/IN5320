283bc95edc569460844a7456c6a42952d143950b
Sander Sigmundstad <sanderls@icloud.com>
2022-09-07 12:12:12 +0200

    Added support for searching the list of countries

--- list.js
+++ list.js
@@ -1,14 +1,16 @@
 const append_item = () => {
     var node = document.createElement("li");
-    var country = document.getElementById("country");
-    var button_node = document.createElement("button");
 
+    var country = document.getElementById("country");
+    var textnode = document.createTextNode(country.value);
+
+    var button_node = document.createElement("button");
     button_node.setAttribute("onclick", "this.parentNode.remove()");
     button_node.setAttribute("class", "cross-button fa-solid fa-circle-xmark");
 
-    var textnode = document.createTextNode(country.value);
     node.appendChild(textnode);
     node.appendChild(button_node);
+
     document.getElementById("list").appendChild(node);
     country.value = '';
 }
@@ -20,4 +22,21 @@ const check_starts_with = (element, searchWord) =>{
 const find_matches = (list, searchWord) => {
     return list.filter(element => check_starts_with(element, searchWord));
 }
+
+const filter_list = () => {
+    var search = document.getElementById("search");
+    var list = document.getElementById("list");
+    var list_items = list.getElementsByTagName("li");
+    var matches = find_matches(Array.from(list_items).map(element => element.innerText), search.value);
+
+    for (i = 0; i < list_items.length; i++) {
+        if (matches.includes(list_items[i].innerText)) {
+            list_items[i].style.display = "";
+          } else {
+            list_items[i].style.display = "none";
+          }
+    }
+    
+}
+
 
