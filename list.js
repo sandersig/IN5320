bd4d9876d1db6c0485e92c3cacc11d0f36cd55b4
Sander Sigmundstad <sanderls@icloud.com>
2022-09-07 11:24:45 +0200

    Added functions for finding elements matching a searchword from a list

--- list.js
+++ list.js
@@ -1,14 +1,23 @@
-function append_item() {
+const append_item = () => {
     var node = document.createElement("li");
     var country = document.getElementById("country");
-    var button = document.createElement("button");
+    var button_node = document.createElement("button");
 
-    button.setAttribute("onclick", "this.parentNode.remove()");
-    button.setAttribute("class", "cross-button fa-solid fa-circle-xmark");
+    button_node.setAttribute("onclick", "this.parentNode.remove()");
+    button_node.setAttribute("class", "cross-button fa-solid fa-circle-xmark");
 
     var textnode = document.createTextNode(country.value);
     node.appendChild(textnode);
-    node.appendChild(button);
+    node.appendChild(button_node);
     document.getElementById("list").appendChild(node);
     country.value = '';
 }
+
+const check_starts_with = (element, searchWord) =>{
+    return element.toLowerCase().startsWith(searchWord.toLowerCase());
+}
+
+const find_matches = (list, searchWord) => {
+    return list.filter(element => check_starts_with(element, searchWord));
+}
+
