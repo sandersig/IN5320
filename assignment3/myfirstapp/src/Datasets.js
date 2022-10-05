161b9b603cf03e5b3be43d26d060b7dc25ba902b
Sander Sigmundstad <sanderls@icloud.com>
2022-10-05 13:53:35 +0200

    Datasets.js now shows a table of the clicked menuitem

--- assignment3/myfirstapp/src/Datasets.js
+++ assignment3/myfirstapp/src/Datasets.js
@@ -1,33 +1,68 @@
 import React from "react";
+import { useDataQuery } from '@dhis2/app-runtime'
+import { Menu, MenuItem, Table, TableHead, TableBody, TableCell, TableRowHead, TableRow, TableCellHead } from '@dhis2/ui'
+import { useState} from "react";
 
-export function Datasets() {
-
-  const request = {
-    request0: {
-      resource: "/dataSets",
-      params: {
-        fields: "id,displayName,created",
-        paging: "false"
-      }
+const request = {
+  request0: {
+    resource: "/dataSets",
+    params: {
+      fields: "id,displayName,created",
+      paging: "false"
     }
   }
-  
-  const sendRequest = () => {
+}
+
+export function Datasets() {
       const { loading, error, data } = useDataQuery(request)
-      if (error) {
-          return <span>ERROR: {error.message}</span>
-      }
-  
-      if (loading) {
-          return <span>Loading...</span>
-      }
-  
-      if (data) {
-         console.log("API response:",data)
-         //To-do: return a component using the data response 
-      }
-  }
-  
-  return <h1>Datasets</h1>
+      const [ row, changeActiveRow] = useState("");
+
+        if (error) {
+            return <span>ERROR: {error.message}</span>
+        }
+    
+        if (loading) {
+            return <span>Loading...</span>
+        }
+
+        const handleClick = (s) => {
+          changeActiveRow(s);
+        }
+
+        if (data) {
+          console.log("API response:",data)
+          return (
+            <div>
+              <Menu>
+                {data.request0.dataSets.map((dataset) => (
+                  <MenuItem onClick={() => handleClick(dataset.displayName)} key={dataset.id} label={dataset.displayName} />
+                ))}
+              </Menu>
+              <Table>
+                <TableHead>
+                    <TableRowHead>
+                        <TableCellHead>Display Name</TableCellHead>
+                        <TableCellHead>ID</TableCellHead>
+                        <TableCellHead>Creation Date</TableCellHead>
+                    </TableRowHead>
+                  </TableHead>
+                    <TableBody>
+                    {data.request0.dataSets.map(dataSets => {
+                      if(dataSets.displayName === row) {
+                      return (
+                      <TableRow key={dataSets.id}>
+                        <TableCell>{dataSets.displayName}</TableCell>
+                        <TableCell>{dataSets.id}</TableCell>
+                        <TableCell>{dataSets.created}</TableCell>
+                      </TableRow>
+                      )
+                      }
+                  })}  
+                    </TableBody>
+              </Table>
+            </div>   
+              
+          );
+        }
 
 }
